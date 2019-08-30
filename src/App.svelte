1057744e2f34245354338f669c7ea9f907ad3be3
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-30 14:03:41 -0700

    Display wins in a grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,36 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte'
+	import axios from 'axios'
+	let win = ""
+	$: wins = []
+
+	
+	const getWins = () => {
+		axios
+			.get(`https://small-wins-819.herokuapp.com/wins`)
+			.then(response => {
+				wins = response.data
+				
+			})
+	}
+	onMount(getWins())
 </script>
 
+
 <style>
-	h1 {
-		color: purple;
+	h4 {
+		color: green;
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="container">
+<h1 class="py-3">Hello Wins</h1>
+<div class="grid-4">
+{#each wins as win}
+	<div class="card">
+			<h4 class="py-3">{win.description}</h4>
+			<p>{win.createdAt.substring(0,10)}</p>
+	</div>
+{/each}
+</div>
+</div>
