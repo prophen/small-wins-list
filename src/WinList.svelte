6efca45b7bb59af1729e9a4d6273e4338cb8ab52
Evan Schultz <evanschultz@Evans-MacBook-Pro-2.local>
2019-12-06 13:12:11 -0500

    created a WinList card that is responsible for displaying the list of wins, and having the App pass in the list to the WinList component
    might look into making the 'card' more reusable in the next commit

---
+++ src/WinList.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let wins;
+</script>
+
+<style>
+  h4 {
+    color: green;
+  }
+</style>
+
+<div class="grid-3">
+  {#if wins != undefined}
+    {#each wins as win}
+      <div class="card">
+        <h4 class="py-3">{win.description}</h4>
+        <p>{win.createdAt.substring(0, 10)}</p>
+      </div>
+    {/each}
+  {/if}
+</div>
